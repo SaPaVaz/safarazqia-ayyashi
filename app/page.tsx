607da7961cd11f1a62aa5553be00ea1453d592ad
SaPaVaz <safarazqiaa@gmail.com>
2026-08-23 15:00:09 +0700

    template tailwind

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import About from '@/components/About'
+import Hero from '@/components/Hero'
+import Learning from '@/components/Learning'
+import Project from '@/components/Project'
+import Stats from '@/components/Stats'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+       <About />
+      <Stats />
+        <Project />
+       <Learning />
+      <Footer />
+     
+    </>
+  );
+}
